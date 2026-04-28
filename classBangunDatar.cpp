b88b53e9e5753a6e3fe5554ba2ace6a8a72daed3
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-28 09:31:54 +0700

    menambahkan void menampilkan data dari class mahasiswa

--- classBangunDatar.cpp
+++ classBangunDatar.cpp
@@ -6,5 +6,11 @@ public:
     int NIM;
     string nama;
     int nilai;
-
+    
+    void printData()
+    {
+        cout << "NIM: " << NIM << endl;
+        cout << "Nama: " << nama << endl;
+        cout << "Nilai: " << nilai << endl;
+    }
 }
